9d0eeec042832a98a39a3e978eaf74b5c63e0c8c
Yann Rouzaud <yann.rouzaud@gmail.com>
2022-03-17 16:10:26 +0100

    Initial Commit

---
+++ LeBaluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBaluchon
+//
+//  Created by Yann Rouzaud on 17/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
